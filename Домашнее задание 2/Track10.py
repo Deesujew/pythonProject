1839bcb5e74b50f814cfe8968fd94b1cabc69897
Deesujew <d.sujew@gmail.com>
2023-02-09 14:42:00 +0300

    add Track14

---
+++ Домашнее задание 2/Track10.py
@@ -0,0 +1,13 @@
+n = int(input('Введите количество монеток: '))
+count = 0
+for i in range(n):
+    temp = int(input())
+    if temp == 0:
+        count += 1
+if n / 2 > count:
+    print(count)
+else:
+    print(n-count)
+
+
+
